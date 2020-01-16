5cc0530a47e2575250aea845cea96e20b5fffba4
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-01-16 00:10:09 -0600

    added clear grid button

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,27 @@
-const container = document.querySelector('#container');
+gridNum = prompt('Choose a Grid Size between 0 and 101')
 
-    for (i = 1; i <= 256; i++) {
+const container = document.querySelector('#container');
+    container.style.backgroundColor = 'silver';
+    container.style.width = "640px";
+    container.style.height = "640px";
+    container.style.display = "grid";
+    container.style.margin = "auto";
+    container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+    container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+
+    gridArea = gridNum ** 2;
+
+    for (i = 1; i <= gridArea; i++) {
     const gridBox = document.createElement('div');
-        gridBox.classList.add('gridBox')
+        gridBox.classList.add('gridBox');
         container.appendChild(gridBox);
 
         gridBox.addEventListener('mouseover', () => {
             gridBox.classList.add('hover');
         });
     };
+
+    const clearGrid = document.querySelector('#clearGrid');
+    clearGrid.addEventListener('click', () => {
+        window.location.reload();
+    });
