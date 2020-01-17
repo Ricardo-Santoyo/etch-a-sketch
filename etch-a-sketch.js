a69a4bfb77bbb05a22a65669971a54ebb9bc9ac4
Ricardo Santoyo <ricardosantoyoch@gmail.com>
2020-01-17 17:24:55 -0600

    added function gradientValue which becomes 10%  more  opaque when cursor hover over it

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -4,14 +4,14 @@ function createGrid(gridNum) {
 
     input(gridNum);
 
-const container = document.querySelector('#container');
-    container.style.backgroundColor = 'silver';
-    container.style.width = "640px";
-    container.style.height = "640px";
-    container.style.display = "grid";
-    container.style.margin = "auto";
-    container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
-    container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
+    const container = document.querySelector('#container');
+        container.style.backgroundColor = 'silver';
+        container.style.width = "640px";
+        container.style.height = "640px";
+        container.style.display = "grid";
+        container.style.margin = "auto";
+        container.style.gridTemplateRows = `repeat(${gridNum}, 1fr)`;
+        container.style.gridTemplateColumns = `repeat(${gridNum}, 1fr)`;
 
     gridArea = gridNum ** 2;
 
@@ -24,14 +24,24 @@ const container = document.querySelector('#container');
             gridBox.classList.add('hover');
         });
 
-        const rgb = document.querySelector('#rgb');
-        rgb.addEventListener('click', () => {
-        gridBox.addEventListener('mouseover', () => {
-        gridBox.classList.remove('hover');
-        gridBox.classList.add('hoverRGB')
-        gridBox.style.backgroundColor = randomRGB();
-    });
-});
+        const rgb = document.querySelector('.rgb');
+            rgb.addEventListener('click', () => {
+                rgb.classList.add('rgbSelected');
+                gradient.classList.remove('gradientSelected');
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.backgroundColor = randomRGB();
+                gridBox.style.opacity = "1";
+            });
+        });
+
+        const gradient = document.querySelector('.gradient');
+            gradient.addEventListener('click', () => {
+                gradient.classList.add('gradientSelected');
+                rgb.classList.remove('rgbSelected');
+            gridBox.addEventListener('mouseover', () => {
+                gridBox.style.backgroundColor = gradientValue(gridBox);
+            });
+        });
     };
 };
 
@@ -44,11 +54,17 @@ clearGrid.addEventListener('click', () => {
 function input(gridNum) {
     if (gridNum < 1 || gridNum > 100 || isNaN(gridNum) == true) {
         createGrid(prompt('Choose a Grid Size between 0 and 101'));
-    }
-}
+    };
+};
 
-function randomRGB () {
+function randomRGB() {
     return "rgb(" + Math.floor(Math.random() * 255)
     + ", " + Math.floor(Math.random() * 255)
     + ", " + Math.floor(Math.random() * 255) + ")";
-}
+};
+
+function gradientValue(gridBox) {
+    let opacity = Number(gridBox.style.opacity);
+    gridBox.style.backgroundColor = 'black';	
+	gridBox.style.opacity = opacity + 0.1;
+};
